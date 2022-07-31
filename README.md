d07d5974c12291fa7c1559f9bc0938312f7ed8f8
haruiswaifu <haruiswaifu@protonmail.com>
2022-07-31 13:45:43 +0100

    write script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usage
+`go run main.go -c "{command}"`
+
+This will run the specified command, output all of its stderr and stdout and if it exits, it'll rerun the command automatically.
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+	sleepSeconds := flag.Int("s", 30, "")
+	command := flag.String("c", "", "")
+	flag.Parse()
+
+	splitCommand := strings.Split(*command, " ")
+
+	cmd := exec.Command(splitCommand[0], splitCommand[1:]...)
+	fmt.Println(cmd)
+
+	for {
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			wrappedErr := fmt.Errorf("failed at cmd.StdoutPipe: %w", err)
+			printMessage(wrappedErr.Error(), true)
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			wrappedErr := fmt.Errorf("failed at cmd.StderrPipe: %w", err)
+			printMessage(wrappedErr.Error(), true)
+		}
+		p := &pipes{
+			stderr,
+			stdout,
+		}
+		doneLogging := make(chan bool, 1)
+		logCommandOutput(doneLogging, p)
+
+		err = cmd.Start()
+		if err != nil {
+			wrappedErr := fmt.Errorf("failed to start underlying program: %w", err)
+			printMessage(wrappedErr.Error(), true)
+		}
+
+		err = cmd.Wait()
+		doneLogging <- true
+		if err != nil {
+			wrappedErr := fmt.Errorf("underlying program exited: %w", err)
+			printMessage(wrappedErr.Error(), true)
+		}
+
+		time.Sleep(time.Duration(*sleepSeconds) * time.Second)
+	}
+}
+
+func printMessage(message string, fatal bool) {
+	fmt.Printf("%s%s", message, "\n")
+	if fatal {
+		os.Exit(1)
+	}
+}
+
+type pipes struct {
+	stderr io.ReadCloser
+	stdout io.ReadCloser
+}
+
+func logCommandOutput(done <-chan bool, p *pipes) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			stdoutOutput, err := io.ReadAll(p.stdout)
+			maybeStdout := string(stdoutOutput)
+			if err != nil {
+				wrappedErr := fmt.Errorf("failed at stdout read: %w", err)
+				printMessage(wrappedErr.Error(), false)
+			}
+			if len(maybeStdout) > 0 {
+				message := fmt.Sprintf("new stdout: %s", maybeStdout)
+				printMessage(message, false)
+			}
+			stderrOutput, err := io.ReadAll(p.stderr)
+			maybeStderr := string(stderrOutput)
+			if err != nil {
+				wrappedErr := fmt.Errorf("failed at stderr read: %w", err)
+				printMessage(wrappedErr.Error(), false)
+			}
+			if len(maybeStderr) > 0 {
+				message := fmt.Sprintf("new stderr: %s", maybeStdout)
+				printMessage(message, false)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
